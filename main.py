e5c37c6f8f3d416868b7053a805164f4345946bb
Türkalp Burak KAYRANCIOĞLU <bkayranci@gmail.com>
2020-10-12 01:49:30 +0300

    gunicorn

--- main.py
+++ main.py
@@ -1,9 +1,33 @@
-import re
 import subprocess
 
-from fastapi import FastAPI
-from starlette.responses import RedirectResponse, JSONResponse
+from flask import Flask, redirect, request, jsonify, make_response
 
-app = FastAPI()
+app = Flask(__name__)
 
-from youtube_downloader.main import *
+
+def is_valid_url(url):
+    import re
+    regex = re.compile(
+        r'^https?://'  # http:// or https://
+        r'(?:(?:[A-Z0-9](?:[A-Z0-9-]{0,61}[A-Z0-9])?\.)+[A-Z]{2,6}\.?|'  # domain...
+        r'localhost|'  # localhost...
+        r'\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3})'  # ...or ip
+        r'(?::\d+)?'  # optional port
+        r'(?:/?|[/?]\S+)$', re.IGNORECASE)
+    return url is not None and regex.search(url)
+
+
+@app.route("/")
+def home_view():
+    url = request.args.get('url')
+    download = request.args.get('download')
+    if not is_valid_url(url):
+        return make_response((jsonify({
+            'detail': 'Not Valid Url'
+        }), 422))
+
+    cmd = subprocess.run(['youtube-dl', '-f', 'best', '--get-url', url], stdout=subprocess.PIPE)
+    result = str(cmd.stdout).lstrip("b'").rstrip("\\n'")
+    if download != 'false':
+        return redirect(result)
+    return {'url': result}
--- youtube_downloader/main.py
+++ youtube_downloader/main.py
@@ -5,16 +5,6 @@ from starlette.responses import RedirectResponse, JSONResponse
 from main import app
 
 
-def is_valid_url(url):
-    import re
-    regex = re.compile(
-        r'^https?://'  # http:// or https://
-        r'(?:(?:[A-Z0-9](?:[A-Z0-9-]{0,61}[A-Z0-9])?\.)+[A-Z]{2,6}\.?|'  # domain...
-        r'localhost|'  # localhost...
-        r'\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3})'  # ...or ip
-        r'(?::\d+)?'  # optional port
-        r'(?:/?|[/?]\S+)$', re.IGNORECASE)
-    return url is not None and regex.search(url)
 
 
 @app.get("/")
